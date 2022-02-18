b253deada5a62563fb021fc4ced1888b54fd9b7e
september21st <tsuilook@gmail.com>
2022-02-18 23:20:48 +0900

    [실습] 쇼핑목록앱 버튼클릭 추가

--- chapter2/exercise - shopping list/main.js
+++ chapter2/exercise - shopping list/main.js
@@ -2,18 +2,23 @@
 
 const $input = document.querySelector('.input')
 const $items = document.querySelector('.items')
+const $addBtn = document.querySelector('.add-btn')
 
 $input.addEventListener('keypress', (e) => {
   if (!e.target.value) return
   if (e.key === 'Enter') {
-    const item = createItem(e.target.value)
-    console.log(item)
-    $items.appendChild(item)
+    addList(esd.target.value)
     e.target.value = ''
   }
 })
 
-function createItem(value) {
+$addBtn.addEventListener('click', (e) => {
+  if (!$input.value) return
+  addList($input.value)
+  $input.value = ''
+})
+
+function addList(value) {
   const newLi = document.createElement('li')
   newLi.className = 'item'
   newLi.innerHTML = `  
@@ -21,5 +26,5 @@ function createItem(value) {
     <button class="delete-btn">
       <i class="fa fa-solid fa-trash " data-icon="trash"></i>
     </button>`
-  return newLi
+  $items.appendChild(newLi)
 }
