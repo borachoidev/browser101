08d2ae9703eeaee34f463ce76b7d28b9cdff0600
september21st <tsuilook@gmail.com>
2022-02-18 23:14:57 +0900

    [실습] 쇼핑목록앱 리스트추가

--- chapter2/exercise - shopping list/main.js
+++ chapter2/exercise - shopping list/main.js
@@ -1,1 +1,25 @@
 'use strict'
+
+const $input = document.querySelector('.input')
+const $items = document.querySelector('.items')
+
+$input.addEventListener('keypress', (e) => {
+  if (!e.target.value) return
+  if (e.key === 'Enter') {
+    const item = createItem(e.target.value)
+    console.log(item)
+    $items.appendChild(item)
+    e.target.value = ''
+  }
+})
+
+function createItem(value) {
+  const newLi = document.createElement('li')
+  newLi.className = 'item'
+  newLi.innerHTML = `  
+    <span calss="item-description">${value}</span>
+    <button class="delete-btn">
+      <i class="fa fa-solid fa-trash " data-icon="trash"></i>
+    </button>`
+  return newLi
+}
