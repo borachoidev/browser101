a7308a5be973bea199d6dfcf74b79e7e9f314bac
september21st <tsuilook@gmail.com>
2022-02-23 19:52:17 +0900

    [실습/리팩토링] game 클래스

--- exercise-getCarrot/src/game.js
+++ exercise-getCarrot/src/game.js
@@ -1,11 +1,123 @@
+import * as sound from './sound.js'
+import Field from './field.js'
 export default class Game {
-  constructor() {
-    this.gameBtn = document.querySelector('.game__button')
+  constructor(gameDuration, carrotCount, bugCount) {
+    this.gameDuration = gameDuration
+    this.carrotCount = carrotCount
+    this.bugCount = bugCount
+
     this.gameTimer = document.querySelector('.game__timer')
     this.gameScore = document.querySelector('.game__score')
+    this.gameBtn = document.querySelector('.game__button')
+    this.gameBtn.addEventListener('click', () => {
+      if (this.started) {
+        this.stop()
+      } else {
+        this.start()
+      }
+    })
+    this.gameField = new Field(carrotCount, bugCount)
+    this.gameField.setClickListener(this.onItemClick)
 
     this.started = false
     this.score = 0
     this.timer
+  }
+  setGameStopListener(onGameStop) {
+    this.onGameStop = onGameStop
+  }
+  start() {
+    this.started = true
+    this.initGame()
+    this.showStopButton()
+    this.showTimerAndScore()
+    this.startGameTimer()
+    sound.playBackground()
+  }
+
+  stop() {
+    this.started = false
+    this.stopGameTimer()
+    this.hideGameButton()
+
+    sound.alertSound()
+    sound.stopBackground()
+    this.onGameStop && this.onGameStop('cancel')
+  }
+
+  finish(win) {
+    this.started = false
+    this.hideGameButton()
+    if (win) sound.playWin()
+    else sound.playBug()
+    this.stopGameTimer()
+    sound.stopBackground()
+    this.onGameStop && this.onGameStop(win ? 'win' : 'lose')
+  }
+
+  onItemClick = (item) => {
+    if (!this.started) {
+      return
+    }
+
+    if (item == 'carrot') {
+      this.score++
+      this.updateScoreBoard()
+
+      if (this.score === this.carrotCount) {
+        this.finish(true)
+      }
+    } else if (item == 'bug') {
+      this.finish(false)
+    }
+  }
+
+  showStopButton() {
+    const icon = this.gameBtn.querySelector('.fa')
+    icon.classList.add('fa-stop')
+    icon.classList.remove('fa-play')
+    this.gameBtn.style.visibility = 'visible'
+  }
+
+  hideGameButton() {
+    this.gameBtn.style.visibility = 'hidden'
+  }
+
+  showTimerAndScore() {
+    this.gameTimer.style.visibility = 'visible'
+    this.gameScore.style.visibility = 'visible'
+  }
+
+  initGame() {
+    this.score = 0
+    this.gameScore.innerHTML = this.carrotCount
+    this.gameField.init()
+  }
+
+  updateScoreBoard() {
+    this.gameScore.innerHTML = this.carrotCount - this.score
+  }
+
+  startGameTimer(time) {
+    let remainingTimeSec = this.gameDuration
+    this.updateTimerText(remainingTimeSec)
+    this.timer = setInterval(() => {
+      if (remainingTimeSec <= 0) {
+        clearInterval(this.timer)
+        this.finish(this.carrotCount === this.score)
+        return
+      }
+      this.updateTimerText(--remainingTimeSec)
+    }, 1000)
+  }
+
+  updateTimerText(time) {
+    const minutes = Math.floor(time / 60)
+    const seconds = time % 60
+    this.gameTimer.innerHTML = `${minutes}:${seconds}`
+  }
+
+  stopGameTimer() {
+    clearInterval(this.timer)
   }
 }
